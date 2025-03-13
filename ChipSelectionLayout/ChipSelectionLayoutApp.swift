afe988a6e75f05e5446b30091b68fcbd1699545c
harsh <bighugscafe@gmail.com>
2025-03-13 18:08:04 +0530

    Initial Commit

---
+++ ChipSelectionLayout/ChipSelectionLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipSelectionLayoutApp.swift
+//  ChipSelectionLayout
+//
+//  Created by harsh  on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ChipSelectionLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChipSelectionLayout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChipSelectionLayout
+//
+//  Created by harsh  on 13/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
