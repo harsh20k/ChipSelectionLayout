0b131a64a2eba50294b7cb6009efb9a6a5b747ea
harsh <bighugscafe@gmail.com>
2025-03-15 12:38:47 +0530

    Repository added

--- ChipSelectionLayout/ContentView.swift
+++ ChipSelectionLayout/ContentView.swift
@@ -7,16 +7,54 @@
 
 import SwiftUI
 
+/// Sample Data
+let tags: [String] = ["IOS 14", "SwiftUI", "macOS", "watchOS", "tvOS", "Xcode", "AppKit", "Cocoa", "Python", "IOS", "C++", "Objective-C", "Go", "R", "SQL", "DBMS"]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationStack {
+			VStack {
+				ChipsView(tags: tags) { tag, isSelected in
+					/// Your Custom View
+					ChipView(tag, isSelected: isSelected)
+					
+				} didChangeSelection: { selection in
+					print(selection)
+				}
+				.padding(10)
+				.background(.gray.opacity(0.1), in: .rect(cornerRadius: 20))
+			}
+			.padding(15)
+			.navigationTitle("Chips Selection")
+		}
     }
+	
+	@ViewBuilder
+	func ChipView(_ tag: String, isSelected: Bool) -> some View {
+		HStack(spacing: 10) {
+			Text(tag)
+				.font(.callout)
+				.foregroundStyle(isSelected ? .white : Color.primary)
+			
+			if isSelected {
+				Image(systemName: "checkmark.circle.fill")
+					.foregroundStyle(.white)
+			}
+		}
+		.padding(.horizontal, 12)
+		.padding(.vertical, 8)
+		.background {
+			ZStack {
+				Capsule()
+					.fill(.background)
+					.opacity(!isSelected ? 1 : 0)
+				
+				Capsule()
+					.fill(.green.gradient)
+					.opacity(isSelected ? 1 : 0)
+			}
+		}
+	}
 }
 
 #Preview {
